d1d7f2ea0243f7bbbe62477a42418d116cb859dd
decorb_n <decorb_n@etna-alternance.net>
2021-03-13 19:05:58 +0100

    fix(option): eslint option fixes

--- web-app/.eslintrc.js
+++ web-app/.eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
     ],
     "rules": {
       "semi": ["error", "always"],
-      "quotes": ["error", "simple", { "avoidEscape": true }]
+      "quotes": ["error", "double"]
     }
 };
